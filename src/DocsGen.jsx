1b95b4e222741b400a67c08e50cd9345c438ef73
Giridhar Nair <giridhar.r.nair@gmail.com>
2023-05-13 18:29:06 -0500

    Added responsiveness

--- src/DocsGen.jsx
+++ src/DocsGen.jsx
@@ -79,7 +79,7 @@ function DocsGen () {
 
   const buttonClass = classNames(
     'bg-gray-500',
-    'w-[100vh]',
+    'md:w-[100vh] w-[40vh]',
     'text-center',
     'h-[5vh]',
     'hover:bg-green-600',
@@ -200,7 +200,7 @@ function DocsGen () {
       <p className='-py-2 text-xs hover:text-green-600 text-white text-center'>
         Programming Language Detected: {language}
       </p>
-      <div className="flex-col w-[100vh] mx-auto py-5 space-y-2">   
+      <div className="flex-col md:w-[100vh] w-[40vh] mx-auto py-5 space-y-2">   
         <Tooltip
           title={"Can't upload files during generation"}
           disabled={!loading}
@@ -208,7 +208,7 @@ function DocsGen () {
         >
           <input
             title="Upload files"
-            className="m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-green-600 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-gray-500 dark:file:text-neutral-100 dark:focus:border-primary"
+            className="m-0 w-full block min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-green-600 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-gray-500 dark:file:text-neutral-100 dark:focus:border-primary"
             type="file"
             onChange={handleFileSelect}
             disabled={loading}
